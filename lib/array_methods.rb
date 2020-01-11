ad8b362765cc3b169c336b3a0cf302b6c00341de
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-11 23:43:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort()[array.length -1]
+  array.sort.last
 end
 
 def find_min_value(array)
-  array.sort()[0]
+  array.sort.first
 end
